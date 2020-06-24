1e67f79c6ea097725a666ceac3c35b1b09fd1fbc
Saurav Mohan V <sauravmohan2@gmail.com>
2020-06-24 16:01:21 +0530

    Simultaneous roll Button

--- lib/main.dart
+++ lib/main.dart
@@ -28,26 +28,70 @@ class _DicePageState extends State<DicePage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Expanded(
-            child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  leftDieNumber = Random().nextInt(5) + 1;
-                });
-              },
-              child: Image.asset('images/dice$leftDieNumber.png'),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        leftDieNumber = Random().nextInt(5) + 1;
+                      });
+                    },
+                    child: Image.asset('images/dice$leftDieNumber.png'),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        rightDieNumber = Random().nextInt(5) + 1;
+                      });
+                    },
+                    child: Image.asset('images/dice$rightDieNumber.png'),
+                  ),
+                ),
+              ],
             ),
           ),
           Expanded(
-            child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  rightDieNumber = Random().nextInt(5) + 1;
-                });
-              },
-              child: Image.asset('images/dice$rightDieNumber.png'),
+            child: Container(
+              width: double.maxFinite,
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                color: Colors.red[600],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                  side: BorderSide(color: Colors.red[800]),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'ROLL',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40.0,
+                      ),
+                    ),
+                    Icon(
+                      Icons.refresh,
+                      size: 40.0,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                onPressed: () {
+                  setState(() {
+                    rightDieNumber = Random().nextInt(5) + 1;
+                    leftDieNumber = Random().nextInt(5) + 1;
+                  });
+                },
+              ),
             ),
           ),
         ],
